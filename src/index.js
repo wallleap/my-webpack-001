6663a438a3c299a9e3fa37fa6c9c9d214697535d
wallleap <15579576761@163.com>
2023-05-15 15:22:13 +0800

    使用第三方库

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { a } from './a.js'
+import axios from 'axios'
+import * as vue from 'vue'
+import React from 'react'
+import ReactDom from 'react-dom'
 
-console.log(a.name)
-console.log(`Hello Webpack`)
+console.log('axios', axios);
+console.log('vue', vue);
+console.log('React', React);
+console.log('ReactDOM', ReactDom);
