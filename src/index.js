1f346f5df13840ed260193c4bb8b0ead96f61864
wallleap <15579576761@163.com>
2023-05-15 16:55:49 +0800

    按需加载测试

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import logo from './images/webpack-logo.png'
 
 const h1 = document.createElement('h1')
 const img = document.createElement('img')
+const btn = document.createElement('button')
 h1.textContent = "Hello CSS"
 img.src = logo
+btn.innerText = 'log a'
 
 document.querySelector('#app').append(h1)
 document.querySelector('#app').append(img)
+document.querySelector('#app').append(btn)
+
+btn.addEventListener('click', () => {
+  import('./a.js').then(({a})=>{
+    console.log(a)
+  },()=>{})
+})
