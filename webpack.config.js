e894e43604090e159378e253f4b3d8f6fc88d7ca
wallleap <15579576761@163.com>
2023-05-15 15:57:56 +0800

    使用 plugin 实现 CSS 单独打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
   mode: 'development',
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
